5fb388e7f667bdf8109af9b6e7ebf2ecf7fa5687
Fengyuan Hu <fengyuan.huu@gmail.com>
2019-09-03 10:14:28 +0100

    switch FTP server

--- data-download.sh
+++ data-download.sh
@@ -6,7 +6,7 @@ echo "This script will download the example datasets."
 echo
 echo "Prerequisite programmes:"
 echo "wget"
-echo "sratoolkit"
+#echo "sratoolkit"
 echo "--------------------------------------------------------------------------"
 echo
 
@@ -18,7 +18,8 @@ echo
 
  
 # Declare a string array with type
-declare -a CmdArray=("wget" "fastq-dump")
+#declare -a CmdArray=("wget" "fastq-dump")
+declare -a CmdArray=("wget")
  
 # Read the array values with space
 for cmd in "${CmdArray[@]}"; do
@@ -33,18 +34,20 @@ done
 
 echo
 echo "--------------------------------------------------------------------------"
-echo "Checking the SRA FTP server is up running..."
+echo "Checking the EBI FTP server is up running..."
 echo "--------------------------------------------------------------------------"
 echo
 
-FTPSTATUS=$(curl -s ftp://ftp-trace.ncbi.nlm.nih.gov/sra/ | grep sra-instant || echo "The site is down")
+#FTPSTATUS=$(curl -s ftp://ftp-trace.ncbi.nlm.nih.gov/sra/ | grep sra-instant || echo "The site is down")
+
+FTPSTATUS=$(curl -s ftp://ftp.sra.ebi.ac.uk/ | grep vol1 || echo "The site is down")
 
 if [[ $FTPSTATUS == *"down"* ]]; then
     echo
-    echo "Error: can not access to the SRA FTP server...please check the network connection." >&2
+    echo "Error: can not access to the EBI FTP server...please check the network connection." >&2
     exit 1
   else
-    echo "The SRA FTP server is up."
+    echo "The EBI FTP server is up."
     echo
 fi
 
@@ -53,16 +56,20 @@ DATADIR=./out/data
 echo "Downloading RNA-Seq data..."
 
 mkdir -p $DATADIR/rna-seq
-wget -nv --tries=10 -O $DATADIR/rna-seq/test.sra ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR160/SRR1605271/SRR1605271.sra 
-fastq-dump --gzip -O $DATADIR/rna-seq/ $DATADIR/rna-seq/test.sra
+#wget -nv --tries=10 -O $DATADIR/rna-seq/test.sra ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR160/SRR1605271/SRR1605271.sra 
+#fastq-dump --gzip -O $DATADIR/rna-seq/ $DATADIR/rna-seq/test.sra
+
+wget -nv --tries=10 -O $DATADIR/rna-seq/test.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR160/001/SRR1605271/SRR1605271.fastq.gz
 
 echo "Dataset downloaded."
 
 echo "Downloading Ribo-Seq data..."
 
 mkdir -p $DATADIR/ribo-seq
-wget -nv --tries=10 -O $DATADIR/ribo-seq/test.sra ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR160/SRR1605304/SRR1605304.sra 
-fastq-dump --gzip -O $DATADIR/ribo-seq/ $DATADIR/ribo-seq/test.sra
+#wget -nv --tries=10 -O $DATADIR/ribo-seq/test.sra ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR160/SRR1605304/SRR1605304.sra 
+#fastq-dump --gzip -O $DATADIR/ribo-seq/ $DATADIR/ribo-seq/test.sra
+
+wget -nv --tries=10 -O $DATADIR/ribo-seq/test.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR160/004/SRR1605304/SRR1605304.fastq.gz
 
 echo "Dataset downloaded."
 
